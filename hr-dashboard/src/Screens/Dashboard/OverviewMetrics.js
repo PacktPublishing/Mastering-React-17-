dc75f9479e76ad2f1f87b9d41527fe76c600f1e6
demo-Ghost <ant.nikolaidis@gmail.com>
2019-05-21 17:21:44 +0300

    Implemented OverviewMetrics component

---
+++ hr-dashboard/src/Screens/Dashboard/OverviewMetrics.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MetricListItem from "./MetricListItem";
+
+const OverviewMetrics = () => {
+  return (
+    <div>
+      <MetricListItem />
+    </div>
+  );
+};
+
+export default OverviewMetrics;
